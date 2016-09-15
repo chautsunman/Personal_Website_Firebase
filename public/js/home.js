5d05352061a9f1b36e8155f0c890b32a31d733dd
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-09-15 17:44:49 +0800

    Add more personal information and skills data to homepage

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,13 @@
-app.controller("home", function($scope, $firebaseObject) {
+app.controller("home", function($scope, $firebaseObject, $window) {
     // header title
     $("header .mdl-layout__title").html("Home");
 
     var infoRef = firebase.database().ref().child("personalInfo");
 
     $firebaseObject(infoRef).$bindTo($scope, "personalInfo");
+
+    // open a link in a new tab
+    $scope.openLink = function(link) {
+        $window.open(link);
+    };
 });
