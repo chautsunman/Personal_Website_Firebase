87e3d06282491c5face209409302ec594f335d89
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-09-16 16:59:59 +0800

    Add work experience data to the work experience page

--- public/js/experience.js
+++ public/js/experience.js
@@ -1,6 +1,10 @@
-app.controller("experience", function($scope) {
+app.controller("experience", function($scope, $firebaseArray) {
     angular.element(document).ready(function() {
         // header title
         $("header .mdl-layout__title").html("Work Experience");
     });
+
+    var experienceRef = firebase.database().ref().child("experience");
+
+    $scope.experience = $firebaseArray(experienceRef);
 });
