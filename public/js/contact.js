21916ee18f021bc72feb326ceebdb6440a613763
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-09-16 21:22:17 +0800

    Add messages list in the contact page

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,4 +1,4 @@
-app.controller("contact", function($scope) {
+app.controller("contact", function($scope, $firebaseArray) {
     angular.element(document).ready(function() {
         // header title
         $("header .mdl-layout__title").html("Contact Me");
@@ -7,4 +7,8 @@ app.controller("contact", function($scope) {
     $scope.submit = function() {
 
     };
+
+    var messageRef = firebase.database().ref().child("messages");
+
+    $scope.messages = $firebaseArray(messageRef);
 });
