a8a81af851919d75c974e42ae6f88cc870de0ca3
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-09-16 22:38:27 +0800

    Add submit function to submit a message in the contact page

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,11 +1,40 @@
-app.controller("contact", function($scope, $firebaseArray) {
+app.controller("contact", function($scope, $firebaseArray, $mdToast) {
     angular.element(document).ready(function() {
         // header title
         $("header .mdl-layout__title").html("Contact Me");
     });
 
-    $scope.submit = function() {
+    function validForm() {
+        if ($scope.name && $scope.email && $scope.subject && $scope.message) {
+            return true;
+        }
 
+        return false;
+    };
+
+    $scope.submit = function() {
+        if (validForm()) {
+            $scope.messages.$add({
+                id: $scope.messages.length,
+                name: $scope.name,
+                email: $scope.email,
+                subject: $scope.subject,
+                body: $scope.message
+            })
+            .then(function(ref) {
+                $mdToast.show(
+                    $mdToast.simple()
+                    .textContent('Submitted message!')
+                    .position("top right")
+                );
+            });
+        } else {
+            $mdToast.show(
+                $mdToast.simple()
+                .textContent('Invalid form!')
+                .position("top right")
+            );
+        }
     };
 
     var messageRef = firebase.database().ref().child("messages");
